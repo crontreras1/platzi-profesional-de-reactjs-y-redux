166961b5e0c79bc039a817f5f042e87f1258a3a6
Cristian Contreras <crontreras1@gmail.com>
2025-02-03 11:51:41 -0500

    class 9. hooks vs connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useEffect } from "react"
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux"
 import { Col } from "antd"
 import { Searcher } from "./components/Searcher"
 import { PokemonList } from "./components/PokemonList"
 import { getPokemon } from "./api"
-import { setPokemons as setPokemonsActions } from "./actions"
+import { setPokemons } from "./actions"
 import logo from './assets/logo.svg'
 import './App.css'
 
-function App({ pokemons, setPokemons }) {
+function App() {
+  const pokemons = useSelector(state => state.pokemons)
+  const dispatch = useDispatch()
+
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon()
 
-      setPokemons(pokemonsRes)
+      dispatch(setPokemons(pokemonsRes))
     } 
 
     fetchPokemons()
@@ -35,8 +38,4 @@ function App({ pokemons, setPokemons }) {
   )
 }
 
-const mapStateToProps = state => ({ pokemons: state.pokemons })
-
-const mapDispatchToProps = dispatch => ({ setPokemons: value =>  dispatch(setPokemonsActions(value)) })
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
